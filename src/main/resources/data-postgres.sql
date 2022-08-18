a6de0557e9af4bf8ac87c01fef9fc1b3c256ee21
R3qUi3M86 <requiem123@gmail.com>
2022-08-19 01:29:58 +0200

    added initial customer reviews to db init

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,4 +1,9 @@
-INSERT INTO public.user(email, password, roles, username)
-VALUES ('admin@mrcook.com', 'pass', 'ADMIN', 'mr-cook');
-INSERT INTO public.user(email, password, roles, username)
-VALUES ('stiepan@mrcook.com', 'pass2', 'CUSTOMER', 'stiepan');
+INSERT INTO public.product_comment(body, create_date, modify_date, rating, title)
+VALUES ('bardzo mi sie podoba :)', timestamp '2022-08-18 22:39:19.485000', timestamp '2022-08-18 22:39:19.485000', 5, 'Super produkt!');
+INSERT INTO public.product_comment(body, create_date, modify_date, rating, title)
+VALUES ('A mi tak srednio. Calskiem spoko opcja dla ludzi co lubia gotowac ale ja jestem w chooy leniwy i mi sie nie chce wiec robi za miske do platkow ;q', timestamp '2022-05-17 15:41:48.000000', timestamp '2022-06-17 15:41:48.000000', 2, 'Taki se...');
+
+INSERT INTO public.user(email, password, roles, username, fk_product_comment)
+VALUES ('admin@mrcook.com', 'pass', 'ADMIN', 'mr-cook', 1);
+INSERT INTO public.user(email, password, roles, username, fk_product_comment)
+VALUES ('stiepan@mrcook.com', 'pass2', 'CUSTOMER', 'stiepan', 2);
