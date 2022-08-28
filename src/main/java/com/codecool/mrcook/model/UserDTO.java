14ed3b474f74b826385e78d5da00133941444297
R3qUi3M86 <requiem123@gmail.com>
2022-08-28 18:58:44 +0200

    improved ProductCommentRestController, now returns userDTO upon succesful CRUD, elevated delete rights for admin user

--- src/main/java/com/codecool/mrcook/model/UserDTO.java
+++ src/main/java/com/codecool/mrcook/model/UserDTO.java
@@ -1,21 +1,25 @@
 package com.codecool.mrcook.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserDTO {
 
     private final long id;
     private final String username;
-    private final boolean member;
-    private final boolean banned;
-    private final boolean productComment;
+    private boolean member;
+    private boolean banned;
+    private String roles;
+    private boolean productComment;
 
     public UserDTO(User user){
         this.id = user.getId();
         this.username = user.getUsername();
         this.member = user.isMember();
         this.banned = user.isBanned();
+        this.roles = user.getRoles();
         this.productComment = user.getProductComment() != null;
     }
 }
